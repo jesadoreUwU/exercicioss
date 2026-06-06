2fa1c11fe7e19b8d4e0ee263fa8cdb70a9367977
jess <pdeportugues2@gmail.com>
2026-06-06 12:54:10 -0300

    Create 2374.c

---
+++ Aula2/2374.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+
+  int pressaodesejada, pressaolida, diferenca;
+  
+  scanf("%d%d", &pressaodesejada, &pressaolida);
+  
+ diferenca = pressaodesejada - pressaolida;
+  
+  printf("%d\n", diferenca);
+ 
+    return 0;
+}
